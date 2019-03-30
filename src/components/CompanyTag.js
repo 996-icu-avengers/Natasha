fb4c32c25024b737f6f831d74ce88d1a26bf52f8
lloyd0408 <lloydzhou@gmail.com>
2019-03-30 23:00:31 +0800

    更新智联站点显示公司标签

--- src/components/CompanyTag.js
+++ src/components/CompanyTag.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import ShadowDOM from 'react-shadow';
-import { findDOMNode  } from 'react-dom';
+import { render, findDOMNode, unmountComponentAtNode } from 'react-dom';
 import { action } from '../utils/store';
 
 export default class CompanyTag extends Component {
   state = {
     name: '',
     tag: '',
+    innerHTML: '',
+  }
+  wrapContainer() {
+    const { name, tag } = this.state;
+    const { show=true } = this.props;
+    const styles = `.icu996 { color: red; border: 1px solid red; border-radius: 3px; margin: 0 3px 0 3px; } .wlb955 { color: green; border: 1px solid green; border-radius: 3px; margin: 0 3px 0 3px; }`
+    return show ? (<span>
+      <span>{name}</span>
+      {show && tag ? <span className={tag}>{tag === 'icu996' ? '996ICU' : '955WLB'}</span> : null }
+      <style type="text/css">{styles}</style>
+    </span>) : null
+  }
+  componentDidUpdate() {
+    render(this.wrapContainer(), this.state.root);
+  }
+  componentWillUnmount() {
+    if (this.state.root) {
+      unmountComponentAtNode(this.state.root);
+    }
   }
   componentDidMount() {
     const node = findDOMNode(this).parentNode;
+    const innerHTML = node.innerHTML;
+    this.setState({ innerHTML})
     const name = node.getAttribute('title');
+    console.log('name', name, innerHTML, node)
     if (name) {
-      this.setState({ name })
+      const mode = 'open';
+      const delegatesFocus = false;
+      const root = node.parentNode.attachShadow ? node.parentNode.attachShadow({ mode, delegatesFocus }) : node.parentNode.createShadowRoot();
+      render(this.wrapContainer(), root)
+      this.setState({ root, name })
       action({ type: 'background/getTagByCompanyName', name}).then(({ err, res, req }) => {
         //console.log('getTagByCompanyName', err, res, req)
         if (!err) {
@@ -23,18 +48,8 @@ export default class CompanyTag extends Component {
     }
   }
   render() {
-    const { name, tag } = this.state;
-    const { show=true } = this.props;
-    const styles = `.icu996 { color: red; border: 1px solid red; border-radius: 3px; margin: 0 3px 0 3px; } .wlb955 { color: green; border: 1px solid green; border-radius: 3px; margin: 0 3px 0 3px; }`
-    return (
-      <ShadowDOM>
-        <span>
-          <span>{name}</span>
-          {show && tag ? <span className={tag}>{tag === 'icu996' ? '996ICU' : '955WLB'}</span> : null }
-          <style type="text/css">{styles}</style>
-        </span>
-      </ShadowDOM>
-    )
+    return <span>{this.state.name}</span>
+    //return <span dangerouslySetInnerHTML={{__html: this.state.innerHTML}} />
   } 
 }
 
--- src/routes/Zhaopin.js
+++ src/routes/Zhaopin.js
@@ -8,9 +8,12 @@ class Zhaopin extends Component {
 
   renderCompanyTag() {
     const { background={show: true, latestRequest: {}} } = this.props;
-    const company_in_sou = document.querySelectorAll('a.company_title');
+    const company_in_sou = document.querySelectorAll('a.company_title, a.companyTitle.listsimple__content__item__box__div__title, button.job-address__jobs__item__company-info__href');
     console.log('company_in_sou', company_in_sou.length);
     [].map.call(company_in_sou, (company_node) => {
+      if (!company_node.getAttribute('title')) {
+        company_node.setAttribute('title', company_node.innerText)
+      }
       if (!company_node.getAttribute('icu996')) {
         company_node.setAttribute('icu996', Date.now())
         //console.log(company_node)
@@ -25,7 +28,7 @@ class Zhaopin extends Component {
   render() {
     const { background={latestRequest: {}} } = this.props;
     this.renderCompanyTag()
-    return <div className="icu996 wlb955">{background.latestRequest.requestId}</div>
+    return <div className="icu996 wlb955" style={{ display: 'none' }}>{background.latestRequest.requestId}</div>
   }
 }
 
