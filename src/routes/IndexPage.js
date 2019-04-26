1094a81c77cdc835edeb70f3d69c53c957548287
lloyd0408 <lloydzhou@gmail.com>
2019-04-26 14:40:38 +0800

    更新外链

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -7,14 +7,18 @@ import icu996 from '../assets/996icu/996icu.png';
 
 function IndexPage({ background={}, dispatch }) {
   const data = [
-    {name: 'Natasha', link: 'https://github.com/996-icu-avengers/Natasha'},
-    {name: '955.WLB', link: 'https://github.com/formulahendry/955.WLB'},
     {name: '996.ICU', link: 'https://github.com/996icu/996.ICU'},
-    {name: '996.LAW', link: 'https://github.com/Y1ran/996.Law'},
-    {name: '996.TSC', link: 'https://github.com/lxlxw/996.TSC'},
+    {name: '955.WLB', link: 'https://github.com/formulahendry/955.WLB'},
     {name: '996.LIST', link: 'https://github.com/fengT-T/996_list'},
+    {name: '996.LAW', link: 'https://github.com/Y1ran/996.Law'},
+    {name: '996.YAOCL', link: 'https://github.com/boycott996/yaocl'},
+    {name: '996.Leave', link: 'https://github.com/623637646/996.Leave'},
     {name: '996.RIP', link: 'https://996.rip/', icon: 'link'},
+    {name: '996.Petition', link: 'https://github.com/xokctah/996.petition'},
+    {name: '996.action', link: 'https://github.com/CPdogson/996action'},
+    {name: '996.avengers', link: 'https://github.com/996-icu-avengers/Natasha'},
     {name: '996.OD', link: 'https://github.com/gnoloehz/996.OD'},
+    {name: 'support.996.ICU', link: 'https://github.com/msworkers/support.996.ICU'},
   ]
   return (
     <div className={styles.popup}>
